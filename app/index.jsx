c0ebcf40180617f36357a0cba9133ae44f4d2bc5
Kia Fathi <kiafathi@me.com>
2014-09-16 14:26:02 -0700

    [FEAT] Update file structure

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,13 @@
 'use strict';
 
 var React = require('react');
-var BarChart = require('./chart/chart.jsx');
+var BarChart = require('./chart/BarChart.jsx');
+console.log('Hi Gulp and Browserify!');
 
 
-
-var mockData = [1,2,3,4,5,6,7,8,9,20,10,11,12,13,14,15,16,17];
-
+var mockData = [10,2,23,4,5,12,7,8,9,20,10,11,12,15,10,15,16,17];
 
 React.renderComponent(
   <BarChart width={600} height={300} dataSet={mockData}/>,
-  document.getElementById('container')
+  document.getElementById('BCcontainer')
 );
-
-console.log('Hi Gulp and Browserify!');
