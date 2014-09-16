1c738b9f8351d9cddeaf60d07162cd429ea2532d
Kia Fathi <kiafathi@me.com>
2014-09-16 14:14:13 -0700

    [FEAT] Add basic BarChart

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,16 @@
 'use strict';
 
 var React = require('react');
-var chart = require('./chart/chart.jsx');
-var myChart = chart('Hi Mom!');
+var BarChart = require('./chart/chart.jsx');
 
 
+
+var mockData = [1,2,3,4,5,6,7,8,9,20,10,11,12,13,14,15,16,17];
+
+
+React.renderComponent(
+  <BarChart width={600} height={300} dataSet={mockData}/>,
+  document.getElementById('container')
+);
 
 console.log('Hi Gulp and Browserify!');
-React.renderComponent(<myChart/>, document.body);
-
